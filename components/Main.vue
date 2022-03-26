2e964f2d4c6d4eb927837a1df800185e62c2ae85
enryleinhard <enryl.einhard@gmail.com>
2022-03-26 19:40:04 +0800

    resume v1.0.0

---
+++ components/Main.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="">
+    <div class="w-full h-screen bg-gray-900">
+      <div class="px-80 py-4 text-white font-mono bg-gray-900">
+        <div class="header flex justify-between">
+          <div class="sub-header-1">
+            <p>enryleinhard</p>
+          </div>
+          <div class="sub-header-2 flex">
+            <div class="pr-6">
+              <a href=""><span>.about</span><span class="text-green-400">()</span></a>
+            </div>
+            <div class="pr-6">
+              <a href=""><span>.projects</span><span class="text-green-400">()</span></a>
+            </div>
+            <div class="pr-6">
+              <a href=""><span>.experiences</span><span class="text-green-400">()</span></a>
+            </div>
+            <div class="pr-6">
+              <a href=""><span>.history</span><span class="text-green-400">()</span></a>
+            </div>
+            <div class="flex">
+              <a href=""><span>.contact</span><span class="text-green-400">()</span></a>
+            </div>  
+          </div>
+        </div>
+        <div class="first text-6xl h-screen pt-72">
+          <div class="py-2 flex">
+            <a href="https://github.com/enryleinhard"><img class="h-10 w-10 mr-6" src="https://i.ibb.co/2gvsrRy/github.png" alt=""></a>
+            <a href="https://www.linkedin.com/in/enryl-einhard-5213541b3/"><img class="h-10 w-10 mr-6" src="https://i.ibb.co/pjszWTN/linkedin.png" alt=""></a>
+            <a href="mailto:enryl.einhard@gmail.com"><img class="h-11 w-11" src="https://i.ibb.co/cX2GPxp/mail.png" alt=""></a>
+          </div>
+          <span class="">
+            enryleinhard
+          </span>
+          <p class="text-xl text-gray-500 py-4">
+            scroll down to explore my resume
+          </p>
+        </div>
+        <div class="h-screen content">
+          <div class="about-1 text-5xl pt-40">
+            <p><span>.about</span><span class="text-green-400">()</span></p>
+          </div>
+          <div class="text-xl about-2 px-8 py-8">
+            <p>
+              hi!, welcome to my website. I'm an aspiring software engineer based in <span class="text-green-400">Hong Kong SAR</span>. Currently, I am a freshman student studying in <span class="text-green-400">City University of Hong Kong</span> majoring in <span class="text-green-400">Data Science</span>. I've been doing programming for the past 3 years. I'm always looking for <span class="text-green-400">new opportunities</span>.
+            </p>
+          </div>
+          <div class="text-xl about-3 px-8 py-8">
+            <p>
+              here are some of my expertise : <span class="text-green-400">Python , Javascript, Vue.JS , GraphQL, Tailwind, MongoDB, Git</span>.
+            </p>
+          </div>
+        </div>
+        <div class="h-screen content">
+          <div class="about-1 text-5xl pt-40">
+            <p><span>.projects</span><span class="text-green-400">()</span></p>
+          </div>
+          <div class="text-xl about-2 px-8 py-8">
+            <p>
+              here is a list of my creations, 
+            </p>
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <a href="https://farmaplus.kemkes.go.id/" class="text-green-400">farmaplus.kemkes.go.id</a> - A prescription drugs and supplies website for the Ministry of Health of the Republic of Indonesia. This website was made to combat the shortage of information about prescription drugs stocks in a particular city, province, or pharmacy during the COVID-19 pandemic. Made using Vue.js, Tailwind, and Strapi.
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <a href="https://yayasanstaging.techkyra.com/" class="text-green-400">yayasanstaging.techkyra.com</a> - A website for a non-profit organization called Yayasan Santa Adeline Kasih based in Indonesia. This website was made to showcase the activities of the organization. Made using Vue.js, Tailwind, and Strapi.
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <a href="https://risetaistaging.techkyra.com/" class="text-green-400">risetaistaging.techkyra.com</a> - A replacement website for a company called riset.ai based in Indonesia. This website was made to introduce the company, and to showcase the activities and products of the company. Made using Vue.js, Tailwind, and Strapi.
+          </div>
+          <div class="text-xl about-2 px-8 p-6"> 
+            - <a href="https://staging.techkyra.com/" class="text-green-400">staging.techkyra.com</a> - A replacement website for a company called Jas Kapital based in Indonesia. This website was made to showcase the activities and the products of the company.  Made using Vue.js, Tailwind, and Strapi.
+          </div>
+        </div>
+        <div class="h-96"></div>
+        <div class="h-screen content">
+          <div class="about-1 text-5xl pt-40">
+            <p><span>.experiences</span><span class="text-green-400">()</span></p>
+          </div>
+          <div class="text-xl about-2 px-8 py-8">
+            <p>
+              here is a list of my programming experiences, 
+            </p>
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <span class="text-green-400">Data Cleaning and Processing Volunteer</span> for the <span class="text-green-400">Indonesian Ministry of Health</span> - cleaned and processed the COVID-19 vaccine cancellation and delay data of Indonesian health workers.
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <span class="text-green-400">Back End Developer Volunteer</span> for the <span class="text-green-400">Jakarta Cathedral</span> - created GraphQL API and Python WhatsApp Bot for Jakarta Cathedral COVID-19 QR code mass attendance system
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <span class="text-green-400">Programming Mentor</span> for <span class="text-green-400">Kolla Education</span> - created the course curriculum and taught Python to students on the Intro to Programming : Python & JavaScript course at Kolla Space Venue 
+          </div>
+          <div class="text-xl about-2 px-8 p-6">
+            - <span class="text-green-400">Programming Mentor</span> for <span class="text-green-400">Kolla Education</span> - created the course curriculum and taught Python to students on the Python: Intro to Programming course online via Zoom
+          </div>
+        </div>
+        <div class="h-60 "></div>
+        <div class="h-screen content">
+          <div class="about-1 text-5xl pt-60">
+            <p><span>.history</span><span class="text-green-400">()</span></p>
+          </div>
+          <div class="text-xl about-2 px-8 py-8">
+            <p>
+              here is a a bit of my life,
+            </p>
+          </div>
+          <div class="text-xl about-2 px-8">
+            <pre>
+<span>[</span><span class="text-yellow-300">'education'</span><span>]</span> = [
+
+  {
+  <span class="text-yellow-300">"high_school"</span> = <span class="text-green-400">'Canisius College'</span>,
+  <span class="text-yellow-300">"major"</span> = <span class="text-green-400">'Natural Science'</span>,
+  <span class="text-yellow-300">"location"</span> = <span class="text-green-400">'Jakarta, Indonesia'</span>,
+  <span class="text-yellow-300">"information"</span> = [<span class="text-green-400">"Part of the Canisius College Computer Club"</span>,<span class="text-green-400">"Magna Cum Laude Awardee"</span>]
+  },
+            </pre>
+          </div>
+          <div class="text-xl about-2 px-8">
+            <pre>
+  {
+  <span class="text-yellow-300">"university"</span> = <span class="text-green-400">'City University of Hong Kong'</span>,
+  <span class="text-yellow-300">"major"</span> = <span class="text-green-400">'Data Science'</span>,
+  <span class="text-yellow-300">"location"</span> = <span class="text-green-400">'Hong Kong SAR'</span>,
+  <span class="text-yellow-300">"information"</span> = [<span class="text-green-400">'Full Tuition Scholarship Awardee'</span>]
+  },
+
+]
+            </pre>
+          </div>
+        </div>
+        <div class="h-80 "></div>
+        <div class="h-screen content">
+          <div class="about-1 text-5xl pt-40">
+            <p><span>.contact</span><span class="text-green-400">()</span></p>
+          </div>
+          <div class="text-xl about-2 px-8 pt-8 pb-4">
+            <p>
+              you can find me on,
+            </p>
+          </div>
+          <div class="text-xl about-2 px-8">
+            <div class="py-2 flex">
+            <a class="flex" href="https://github.com/enryleinhard"><img class="h-10 w-10 mr-6" src="https://i.ibb.co/2gvsrRy/github.png" alt=""></a>
+            <a href="https://www.linkedin.com/in/enryl-einhard-5213541b3/"><img class="h-10 w-10 mr-6" src="https://i.ibb.co/pjszWTN/linkedin.png" alt=""></a>
+            <a href="mailto:enryl.einhard@gmail.com"><img class="h-11 w-11" src="https://i.ibb.co/cX2GPxp/mail.png" alt=""></a>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import gsap
+
+export default {
+  name: 'Main',
+  methods: {
+    set() {
+      const gsap = this.$gsap
+      var headlines = gsap.utils.toArray(".first, .about-1, .about-2, .about-3");
+      headlines.forEach((elem, i) => {
+      
+      gsap.set(elem, {
+        opacity: 0
+      })
+
+      const tl = gsap.timeline( { 
+        scrollTrigger: {
+          trigger: elem,
+          start: "+=133 80%",
+          end: "+=10 0%",
+          scrub: true,
+          markers: true,
+          toggleActions: "play reverse play reverse",
+        }
+        
+      });
+
+
+      tl
+        .to(elem, { opacity: 1, duration: 0.2 })
+        .to(elem, { opacity: 0, duration: 0.2 }, 0.8 )
+      ;
+    })
+    }
+  },
+  mounted() {
+    this.set()
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Roboto Mono", ...defaultTheme.fontFamily.mono]
+      },
+    },
+  },
+  variants: {
+    extend: {}
+  },
+};
